48ea8092e8440f994ffb5dd84d1ca9a2138ef348
09pipapipa <pipapipa.hold.up@gmail.com>
2024-04-07 15:11:38 +0900

    メインぺージ作成

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,3 +1,12 @@
 <template>
-  <footer class="footer">2024-</footer>
+  <footer class="footer">2024-terms-violation</footer>
 </template>
+
+<style scoped>
+.footer {
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  box-shadow: 0 1px 1px #ddd;
+}
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1 class="title">コンプライアンス動画視聴確認テスト</h1>
+  <div class="title">コンプライアンス動画視聴確認テスト</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+  width: 100%;
+  margin: 0;
+  text-align: center;
+  background-color: #fff;
+  font-family: sans-serif;
+  padding: 10px;
+  font-size: 20px;
+  letter-spacing: 3px;
+}
+</style>
