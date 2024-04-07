14d32e07308d70bda0eeaf0fac40ee5b5c28ea51
09pipapipa <pipapipa.hold.up@gmail.com>
2024-04-07 18:52:58 +0900

    動画埋め込み

--- src/components/MainContents.vue
+++ src/components/MainContents.vue
@@ -2,8 +2,17 @@
 
 <template>
   <div class="top-container">
-    <div class="top-direction"></div>
-    <div class="compliance_video"></div>
+    <div class="top-direction">
+      <p>就業するにあたりコンプライアンス動画の視聴をお願いします。</p>
+      <p>視聴が終了したら下記リンクから確認テストを実施して下さい。</p>
+      <p>テストは制限時間10分、4択全10問となります。</p>
+      <p>テスト終了後の画面は消さずに端末を従業員へ渡してください。</p>
+    </div>
+    <div class="compliance_video">
+      <video controls width="600">
+        <source src="/media/temporary.mp4" type="video/webm" />
+      </video>
+    </div>
     <div class="start-btn">
       <button class="btn-start"></button>
     </div>
