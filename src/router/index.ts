cd99fe5ed399b5e6162c0bad4f2459654663c704
09pipapipa <pipapipa.hold.up@gmail.com>
2024-04-08 11:23:15 +0900

    テストページルート作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainContents from '@/views/MainContents.vue'
+import TestPage from '@/views/TestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'MainContents',
       component: MainContents
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: TestPage
     }
   ]
 })
